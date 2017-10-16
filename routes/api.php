666a7186cd65bde420ff8cbb023473e80123fe71
whereisjefe <jeffbeltran@gmail.com>
2017-10-15 20:54:22 -0700

    Removed boilerplate code

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('links', 'Api\LinkController@index');
 Route::post('links', 'Api\LinkController@store');
 
